429e2584bc526fd318925ee44f2c95f5e7643fd5
mhmdali23 <mhmdali23101@gmail.com>
2024-12-13 14:02:49 +0200

    calc heighest and lowest price after discount through resolver

---
+++ CanddelsBackEnd/Dtos/CategoryDto.cs
@@ -0,0 +1,12 @@
+﻿namespace CanddelsBackEnd.Dtos
+{
+    public class CategoryDto
+    {
+        public int Id { get; set; }
+        public string name {  get; set; }
+        public string description { get; set; }
+        public string? ImageUrl { get; set; }
+
+
+    }
+}
