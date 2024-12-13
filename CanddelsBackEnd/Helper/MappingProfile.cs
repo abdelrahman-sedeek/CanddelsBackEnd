e83927ea8b6cbf14952d67dfa187d4674b074e8b
mhmdali23 <mhmdali23101@gmail.com>
2024-12-13 14:00:35 +0200

    Include Discount

--- CanddelsBackEnd/Helper/MappingProfile.cs
+++ CanddelsBackEnd/Helper/MappingProfile.cs
@@ -2,6 +2,7 @@
 using CanddelsBackEnd.Models;
 using AutoMapper;
 using CanddelsBackEnd.Dtos;
+using System.Runtime.CompilerServices;
 namespace CanddelsBackEnd.Helper
 {
     public class MappingProfile : Profile
@@ -12,22 +13,25 @@ namespace CanddelsBackEnd.Helper
           
             CreateMap<Product, ProductToReturnDto>()
                 .ForMember(dest => dest.HighestPrice, opt => opt.MapFrom(src => GetHighestPrice(src.productVariants)))
-                .ForMember(dest => dest.LowestPrice, opt => opt.MapFrom(src => GetLowestPrice(src.productVariants)));
+                .ForMember(dest => dest.LowestPrice, opt => opt.MapFrom(src => GetLowestPrice(src.productVariants)))
+                .ForMember(dest => dest.HighestPriceAfterDiscount, opt => opt.MapFrom<HighestPriceDiscountResolver>())
+                .ForMember(dest => dest.LowestPriceAfterDiscount, opt => opt.MapFrom<LowesPriceDiscountResolver>());
+                    
 
             CreateMap<Product, ProductToReturnByIdDto>()
-                .ForMember(dest => dest.HighestPrice, opt => opt.MapFrom(src => GetHighestPrice(src.productVariants)))
-                .ForMember(dest => dest.LowestPrice, opt => opt.MapFrom(src => GetLowestPrice(src.productVariants)));
+                .ForMember(dest => dest.HighestPriceAfterDiscount, opt => opt.MapFrom(src => GetHighestPrice(src.productVariants)))
+                .ForMember(dest => dest.LowestPriceAfterDiscount, opt => opt.MapFrom(src => GetLowestPrice(src.productVariants)));
        
             CreateMap<Category, CategoryToReturnDto>();
         }
         private decimal? GetHighestPrice(IEnumerable<ProductVariant> variants) =>
-                variants != null && variants.Any() && variants.Count() > 1
+                variants != null && variants.Any() 
                     ? variants.Max(v => v.Price)
-                    : (decimal?)null;
+                    : null;
         private decimal? GetLowestPrice(IEnumerable<ProductVariant> variants) =>
-            variants != null && variants.Any()
+            variants != null && variants.Any() 
                 ? variants.Min(v => v.Price)
-                : (decimal?)null;
-       
+                : null;
+        
     }
 }
--- CanddelsBackEnd/Specifications/ProductSpesification.cs
+++ CanddelsBackEnd/Specifications/ProductSpesification.cs
@@ -8,11 +8,14 @@ namespace CanddelsBackEnd.Specifications
         {
             AddInclude(X => X.productVariants);
             AddInclude(X =>X.Category);
+            AddInclude(x => x.Discount);
         }
         public ProductSpesification(int id):base(x=>x.Id==id)
         {
             AddInclude(X => X.productVariants);
             AddInclude(X => X.Category);
+            AddInclude(X => X.Discount);
+
         }
         public ProductSpesification(bool IsBestSeller) :base(x=>x.IsBestSeller== IsBestSeller)
         {
