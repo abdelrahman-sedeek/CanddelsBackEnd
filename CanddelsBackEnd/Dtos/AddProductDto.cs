266357733a0a7ad62fc7fccc596bda012299f15e
abdelrahman tolba sedeek <67134107+abdelrahman-sedeek@users.noreply.github.com>
2024-12-28 20:32:53 +0200

    productDashboard

---
+++ CanddelsBackEnd/Dtos/AddProductDto.cs
@@ -0,0 +1,26 @@
+﻿using CanddelsBackEnd.Models;
+using System.ComponentModel;
+
+namespace CanddelsBackEnd.Dtos
+{
+    public class AddProductDto
+    {
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public string? Features { get; set; }
+        public string? Benfits { get; set; }
+        public string? Scent { get; set; }
+
+        public bool IsBestSeller { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public bool IsDailyOffer { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public string? CalltoAction { get; set; }
+        public decimal? DiscountPercentage { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
---
+++ CanddelsBackEnd/Dtos/AddProductVariantsDto.cs
@@ -0,0 +1,13 @@
+﻿using CanddelsBackEnd.Models;
+
+namespace CanddelsBackEnd.Dtos
+{
+    public class AddProductVariantsDto
+    {
+        public decimal Barcode { get; set; }
+        public int StockQuantity { get; set; }
+        public decimal Weight { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
