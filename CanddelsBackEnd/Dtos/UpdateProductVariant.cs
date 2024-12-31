ab6646318a6cb1f98d5eceaaf34d0dff159f397f
mhmdali23 <mhmdali23101@gmail.com>
2024-12-31 08:40:57 +0200

    refactor updateProduct and add validation

---
+++ CanddelsBackEnd/Dtos/UpdateProductVariant.cs
@@ -0,0 +1,12 @@
+﻿namespace CanddelsBackEnd.Dtos
+{
+    public class UpdateProductVariant
+    {
+        public int Barcode { get; set; }
+        public int StockQuantity {  get; set; }
+        public int  Weight { get; set; }
+        public decimal Price { get; set; }
+
+
+    }
+}
---
+++ CanddelsBackEnd/Validations/AllowedImageExtensionsAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CanddelsBackEnd.Validations
+{
+    public class AllowedImageExtensionsAttribute : ValidationAttribute
+    {
+        private readonly string[] _extensions;
+
+        public AllowedImageExtensionsAttribute(string[] extensions)
+        {
+            _extensions = extensions;
+        }
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var file = value as IFormFile;
+
+            if (file != null)
+            {
+                var extension = Path.GetExtension(file.FileName).ToLower();
+
+                if (!_extensions.Contains(extension))
+                {
+                    return new ValidationResult(ErrorMessage ?? $"Invalid file type. Allowed types are: {string.Join(", ", _extensions)}");
+                }
+            }
+            
+            return ValidationResult.Success;
+        }
+
+    }
+}
