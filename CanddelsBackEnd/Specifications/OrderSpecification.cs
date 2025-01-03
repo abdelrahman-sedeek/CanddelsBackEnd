d8283248e5718863af41423668e1ea91061bb5a9
abdelrahman tolba sedeek <67134107+abdelrahman-sedeek@users.noreply.github.com>
2025-01-03 17:03:43 +0200

    order dashboard

---
+++ CanddelsBackEnd/Specifications/OrderSpecification.cs
@@ -0,0 +1,22 @@
+﻿using CanddelsBackEnd.Models;
+
+namespace CanddelsBackEnd.Specifications
+{
+    public class OrderSpecification:BaseSpecification<Order>
+    {
+        public OrderSpecification() {
+            AddInclude(sd => sd.ShippingDetail);
+               
+        
+        }
+        public OrderSpecification(int id):base(x=>x.Id==id)
+        {
+            AddInclude(sd => sd.ShippingDetail);
+
+        }
+        
+            
+        
+
+    }
+}
