d75ab4032fcf5b1d5bada76d72c0121f3be23a77
mhmdali23 <mhmdali23101@gmail.com>
2025-01-19 21:47:50 +0200

    add category in dashboard

---
+++ CanddelsBackEnd/Controllers/ScentControllercs.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CanddelsBackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ScentControllercs : ControllerBase
+    {
+        public ScentControllercs()
+        {
+            
+        }
+    }
+}
